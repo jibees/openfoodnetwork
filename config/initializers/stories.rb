4daf08fb01766a8372ed3ffb10f54de9c8896fab
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-04-09 12:01:16 +0200

    Configure CORS settings for StoriesController
     - Storybook need to access through a GET method on `/rails/stories/**` served by `ViewComponent::Storybook::StoriesController`
     - Configure exact policy with an initialized filter

---
+++ config/initializers/stories.rb
@@ -0,0 +1,9 @@
+module PermissiveCORSHeaders
+
+  def self.before((response))
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET"
+  end
+end
+
+ViewComponent::Storybook::StoriesController.before_filter(PermissiveCORSHeaders) if Rails.env.development?
