3f2f55acd10fb00f34ac8c6b03c67bc901e39bc8
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-04-09 15:35:24 +0100

    Wrap all initializer code in Rails.env check and add comment

--- config/initializers/storybook.rb
+++ config/initializers/storybook.rb
@@ -1,8 +1,14 @@
-module PermissiveCORSHeaders
-  def self.before(response)
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET"
+# Adjust headers to allow running Storybook in development.
+# Uses iframes and doesn't play nicely with CORS checks
+
+if Rails.env.development?
+  module PermissiveCORSHeaders
+    def self.before(response)
+      response.headers["Access-Control-Allow-Origin"] = "*"
+      response.headers["Access-Control-Allow-Methods"] = "GET"
+    end
   end
+
+  ViewComponent::Storybook::StoriesController.before_filter(PermissiveCORSHeaders)
 end
 
-ViewComponent::Storybook::StoriesController.before_filter(PermissiveCORSHeaders) if Rails.env.development?
