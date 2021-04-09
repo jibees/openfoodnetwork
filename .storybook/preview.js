5cf479b08665b8358b928d438be03e29b21b1ce9
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-04-09 15:20:22 +0200

    Serve assets (css+roboto font) for the preview iframe
     - Thus, component can inherit from all CSS rules, rendering is as close as to the truth
     - Still need to import `admin` css file if we want to create components for admin part (and we probably want too). This will probably lead to overlapping problems between all CSS rules...
    - This is a bit hacky, but as our CSS files are packed by rails I didn't find any better solution. We could also use any sass loader, but with this solution we can be confident on the fact that the generated CSS is the one used by the server that finally displays the component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 export const parameters = {
   server: {
+    assetsURL: `http://localhost:3000`,
     url: `http://localhost:3000/rails/stories`,
   },
 };
+
+document.querySelector('head').innerHTML = document.querySelector("head").innerHTML.replaceAll(/\${SERVER_URL}/g, parameters.server.assetsURL)
